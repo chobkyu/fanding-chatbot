f980a80a7e5ec97847d30e297c1907abde2e3919
chobkyu <qudqud97@naver.com>
2025-06-17 15:01:25 +0900

    feat: add prompt

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from langchain_openai import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain.prompts import PromptTemplate
 import os
 
 load_dotenv()
@@ -18,9 +19,26 @@ class Question(BaseModel):
 embedding = OpenAIEmbeddings()
 vectordb = FAISS.load_local("faiss_index", embedding, allow_dangerous_deserialization=True)
 
+prompt = PromptTemplate(
+    input_variables=["context", "question"],
+    template="""
+You are a customer support chatbot.
+Based on the following information (Context), answer the user's question (Question) accurately and politely in korean.
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:"""
+)
+
 qa = RetrievalQA.from_chain_type(
     llm=ChatOpenAI(model="gpt-4.1-nano", temperature=0.7),
-    retriever = vectordb.as_retriever()
+    retriever = vectordb.as_retriever(),
+    chain_type = "stuff",
+    chain_type_kwargs= {"prompt":prompt}
 )
 
 def get_answer(query: str) -> str:
